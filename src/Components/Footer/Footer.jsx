088a2011b8682415331d886061af1705b7c66f46
Alejoo1812 <alejorush66@gmail.com>
2024-01-09 18:40:45 -0300

    entrega final

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import './Footer.css';
+import { RiWhatsappFill, RiInstagramFill, RiFacebookFill, RiTwitterFill, RiLinkedinBoxFill, RiGithubFill } from 'react-icons/ri';
+
+export default function Footer() {
+    const socials = [
+        {
+            id: 1,
+            name: 'Instagram',
+            link: 'https://www.instagram.com/'
+        },
+        {
+            id: 2,
+            name: 'Facebook',
+            link: 'https://www.facebook.com/'
+        },
+        {
+            id: 3,
+            name: 'Twitter',
+            link: 'https://twitter.com/'
+        },
+    ]
+}
